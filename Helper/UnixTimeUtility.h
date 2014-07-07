b7c9398d78e45cce77a3db069956c3599134c493
Allan <cyjian0@gmail.com>
2014-07-07 18:41:03 +0800

    add Helper

---
+++ Helper/UnixTimeUtility.h
@@ -0,0 +1,35 @@
+//
+//  UnixTimeUtility.h
+//  KnowBaby
+//
+//  Created by Allan.Chan on 13-4-25.
+//  Copyright (c) 2013年 Allan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UnixTimeUtility : NSObject
++(float)nowUnixTime;
++(NSString *)getNowTime;
++(NSString *)transformUnixTime:(double)time;
++(NSString *)transformUnixTimeHoursAndMin:(double)time;
+
+/*
+ | bosim 比较当前月和指定月的差值
+ */
++(int)compareDateValue:(NSString *) oneDay withAnotherDay:(NSString *)anotherDay;
+
+/*
+ | 比较两个时间，返回时间的差值
+ | 差值直接是变成字符传
+ */
++ (NSString *)compareDateValueHms:(NSDate *)oneDate withAnotherDay:(NSDate *)anotherDate;
+
++ (NSString *)twoDigit:(int)digit;
+
++ (int)calculateAgeFromDate:(NSDate *)oneDay toDate:(NSDate *)anotherDay;
+
++ (NSDate *)dateFromString:(NSString *)dateString;
+
++ (NSString *)stringFromDate:(NSDate *)date;
+@end
